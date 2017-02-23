a8bbb6e90f805ff344a7cc7d0887b62686fa1cbe
nicothebrush <nicola.riolini@gmail.com>
2017-02-23 18:57:54 +0100

    add procedure for merge pdf

--- label_system_mrp/label_mrp.py
+++ label_system_mrp/label_mrp.py
@@ -90,6 +90,25 @@ class MrpProduction(orm.Model):
     
     _inherit = 'mrp.production'
     
+    def merge_pdf_printer(self, cr, uid, ids, context=None):
+        '''# Loading the pyPdf Library
+        from pyPdf import PdfFileWriter, PdfFileReader
+
+        # Creating a routine that appends files to the output file
+        def append_pdf(input,output):
+            [output.addPage(input.getPage(page_num)) for page_num in range(input.numPages)]
+
+        # Creating an object where pdf pages are appended to
+        output = PdfFileWriter()
+
+        # Appending two pdf-pages from two different files
+        append_pdf(PdfFileReader(open("SamplePage1.pdf","rb")),output)
+        append_pdf(PdfFileReader(open("SamplePage2.pdf","rb")),output)
+
+        # Writing all the collected pages to a file
+        output.write(open("CombinedPages.pdf","wb"))'''
+        return True
+                
     def generate_label_job(self, cr, uid, ids, context=None):
         ''' Generate list of jobs label
         '''
