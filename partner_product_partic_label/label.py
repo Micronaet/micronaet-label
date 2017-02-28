c78147c5b3e2322e1a3a2594be931a07dcc8347c
nicothebrush <nicola.riolini@gmail.com>
2017-02-28 18:46:42 +0100

    check data period

--- partner_product_partic_label/label.py
+++ partner_product_partic_label/label.py
@@ -838,9 +838,13 @@ class ResPartner(orm.Model):
             'record_data_line': line_code,
             #'record_data_period': '%s%s' % (
             #    mrp.date_planned[2:4], mrp.date_planned[5:7]),
-            'record_data_period': datetime.strptime(
-                mrp.date_planned[:10], 
-                DEFAULT_SERVER_DATE_FORMAT).isocalendar()[1],
+            # TODO format YY WW
+            'record_data_period': '%s%02d' % (
+                mrp.date_planned[:4],
+                datetime.strptime(
+                    mrp.date_planned[:10]), 
+                    DEFAULT_SERVER_DATE_FORMAT,
+                    ).isocalendar()[1],
             'record_data_lot': mrp.name.replace('MO', '').lstrip('0'),
             
             # -----------------------------------------------------------------
