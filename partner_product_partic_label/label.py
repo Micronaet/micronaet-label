37999c01364128f43073a3de21acdaaf4e8865b2
nicothebrush <nicola.riolini@gmail.com>
2017-11-22 08:37:35 +0100

    generic bugfix (fast message)

--- partner_product_partic_label/label.py
+++ partner_product_partic_label/label.py
@@ -857,7 +857,7 @@ class ResPartner(orm.Model):
             'record_data_q_x_pack': 
                 int(product.q_x_pack) if product.q_x_pack else '',
             'record_data_q_x_pallet': 
-                product.q_x_pallet or product.item_x_pallet or '',
+                product.q_x_pallet or product.item_per_pallet or '',
             'record_data_dimension': '%sx%sx%s' % (
                 product.height, product.width, product.length) if (
                     product.height and product.width and product.length) else\
