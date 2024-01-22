ac38279f2856e124ef51a3f08c805493c1294db7
nicothebrush <nicola.riolini@gmail.com>
2024-01-22 15:49:40 +0100

    generic bugfix (fast message)

--- ean_single/extra_ean.py
+++ ean_single/extra_ean.py
@@ -18,6 +18,7 @@
 #
 ###############################################################################
 import os
+import pdb
 import sys
 import logging
 import openerp
@@ -87,11 +88,12 @@ class ProductProduct(orm.Model):
     def generate_barcode_ean13_mono(self, cr, uid, ids, context=None):
         """ Call original function with force
         """
+        pdb.set_trace()
         if context is None:
             context = {}
         ctx = context.copy()
         ctx['force_field'] = 'ean13_mono'
-
+        pdb.set_trace()
         return self.generate_barcode_ean13(cr, uid, ids, context=ctx)
 
     _columns = {
